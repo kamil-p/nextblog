c4c770c929ce578662c56f3fee2b37ad51be8168
Kamil Purzynski <kamil.purzynski@gmail.com>
2023-06-25 16:12:18 +0200

    Started displaying blog content through typescript objects.

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -3,6 +3,8 @@ import { PageLayout } from '@/components/Layout'
 import { getBlogSlugs, getBlogBySlug } from '@/lib/blogs'
 import { type GetStaticProps } from 'next'
 import { type Blog } from '@/Interface/Blog'
+import { type ParsedUrlQuery } from 'querystring'
+import Image from 'next/image'
 
 interface Props {
   blog: Blog
@@ -10,7 +12,7 @@ interface Props {
 
 const BlogDetail: NextPage<Props> = ({ blog }) => {
   return (
-    <PageLayout>
+    <PageLayout pageTitle={blog.title}>
         <div className="w-2/3 m-auto">
             {/* Blog Header Starts */}
             <div className="blog-detail-header">
@@ -18,26 +20,26 @@ const BlogDetail: NextPage<Props> = ({ blog }) => {
                     <div className="flex items-center">
                         <div className="flex-shrink-0">
                             <a href="#">
-                                <span className="sr-only">Author Name</span>
+                                <span className="sr-only">{blog.author}</span>
                                 <div className="relative h-10 w-10 !mb-0" >
-                                    {/* <Image
-                priority
-                layout="fill"
-                objectFit="cover"
-                className="rounded-full"
-                src={authorImage} alt=""
-              /> */}
+                                    <Image
+                                        priority
+                                        layout="fill"
+                                        objectFit="cover"
+                                        className="rounded-full"
+                                        src={blog.authorImage} alt=""
+                                    />
                                 </div>
                             </a>
                         </div>
                         <div className="ml-3">
                             <p className="text-sm font-medium text-gray-900 !mb-0">
                                 <a href="#" className="hover:underline">
-                                    Author Name
+                                    {blog.author}
                                 </a>
                             </p>
                             <div className="flex space-x-1 text-sm text-gray-500">
-                                <time dateTime="{date}">2022-10-10</time>
+                                <time dateTime={blog.date}>{blog.date}</time>
                             </div>
                         </div>
                     </div>
@@ -45,14 +47,14 @@ const BlogDetail: NextPage<Props> = ({ blog }) => {
                         {/* Social Links Here */}
                     </div>
                 </div>
-                <h1 className="font-bold text-4xl mb-1">My First Blog</h1>
-                <h2 className="blog-detail-header-subtitle mb-2 text-xl text-gray-600">My first blog description</h2>
+                <h1 className="font-bold text-4xl mb-1">{blog.title}</h1>
+                <h2 className="blog-detail-header-subtitle mb-2 text-xl text-gray-600">{blog.description}</h2>
                 <div className="h-96 bg-black mx-auto w-full relative">
-                    {/* <Image
-        priority
-        layout="fill"
-        objectFit="cover"
-        src={coverImage} alt=""/> */}
+                    <Image
+                        priority
+                        layout="fill"
+                        objectFit="cover"
+                        src={blog.coverImage} alt=""/>
                 </div>
             </div>
             {/* Blog Header Ends */}
@@ -66,7 +68,17 @@ const BlogDetail: NextPage<Props> = ({ blog }) => {
   )
 }
 
-export const getStaticProps: GetStaticProps = (context: any) => {
+interface Params extends ParsedUrlQuery {
+  slug: string
+}
+
+export const getStaticProps: GetStaticProps<Props, Params> = (context) => {
+  if (context.params == null) {
+    return {
+      notFound: true
+    }
+  }
+
   const { slug } = context.params
   const blog = getBlogBySlug(slug)
   return {
